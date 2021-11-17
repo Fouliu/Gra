3da07d19e46a96392b2a97a6946f710395324b29
Jawor Mateusz (ADM) <mjawor_adm@inpost.pl>
2021-11-17 17:36:07 +0100

    mapa csv dodana

---
+++ graphics/tilesets/crates.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="crates" tilewidth="58" tileheight="42" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="58" height="42" source="../terrain/crate.png"/>
+ </tile>
+ <tile id="1">
+  <image width="58" height="42" source="../terrain/crate.png"/>
+ </tile>
+</tileset>
