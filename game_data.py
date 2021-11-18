71159146f0e6d5333e3ca7c3e4d4cd3da2b376f5
Jawor Mateusz (ADM) <mjawor_adm@inpost.pl>
2021-11-18 11:55:18 +0100

    Dodanie obslugi pliku CSV dla terrain

--- game_data.py
+++ game_data.py
@@ -1,10 +1,10 @@
 level_0 = {
-        'terrain': '../levels/0/level_0_terrain.csv',
-        'coins': '../levels/0/level_0_coins.csv',
-        'fg palms': '../levels/0/level_0_fg_palms.csv',
-        'bg palms': '../levels/0/level_0_crates.csv',
-        'enemies': '../levels/0/level_0_enemies.csv',
-        'constraints': '../levels/0/level_0_constraint.csv',
-        'player': '../levels/0/level_0_player.csv',
-        'grass': '../levels/0/level_0_grass.csv'
+        'terrain': '../Gra/levels/0/level_0_terrain.csv',
+        'coins': '../Gra/levels/0/level_0_coins.csv',
+        'fg palms': '../Gra/levels/0/level_0_fg_palms.csv',
+        'bg palms': '../Gra/levels/0/level_0_crates.csv',
+        'enemies': '../Gra/levels/0/level_0_enemies.csv',
+        'constraints': '../Gra/levels/0/level_0_constraint.csv',
+        'player': '../Gra/levels/0/level_0_player.csv',
+        'grass': '../Gra/levels/0/level_0_grass.csv'
           }
--- level.py
+++ level.py
@@ -1,9 +1,9 @@
 import pygame
-from tiles import Tile
+from tiles import Tile, StaticTile
 from settings import tile_size, screen_width
 from player import Player
 from particles import ParticleEffect
-from support import import_csv_layout
+from support import import_csv_layout, import_cut_graphics
 
 
 class Level:
@@ -14,19 +14,32 @@ class Level:
         self.setup_level(level_data)
         self.world_shift = 0
         self.current_x = 0
-        # layout
 
+        # terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
 
+        # grass setup
+        grass_layout = import_csv_layout(level_data['grass'])
+        self.grass_sprites = self.create_tile_group(grass_layout, 'grass')
         # dust
         self.dust_sprite = pygame.sprite.GroupSingle()
         self.player_on_ground = False
 
     def create_tile_group(self, layout, type):
         sprite_group = pygame.sprite.Group()
-        for row in layout:
-            print(row)
+
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                if val != "-1":
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        terrain_tile_list = import_cut_graphics('../Gra/graphics/terrain/terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+                        sprite_group.add(sprite)
 
         return sprite_group
 
@@ -129,9 +142,9 @@ class Level:
         self.dust_sprite.draw(self.display_surface)
 
         # level tiles
+        self.terrain_sprites.draw(self.display_surface)
+        self.terrain_sprites.update(self.world_shift)
         self.tiles.update(self.world_shift)
-        self.tiles.draw(self.display_surface)
-        self.scroll_x()
 
         # player
         self.player.update()
